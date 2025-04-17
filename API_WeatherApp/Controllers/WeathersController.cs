5348cad77af0a0b25694fe1cbdf645c9afd15e8c
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-17 17:41:15 +0300

    Api test and configuration.

---
+++ API_WeatherApp/Controllers/WeathersController.cs
@@ -0,0 +1,22 @@
+﻿using API_WeatherApp.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_WeatherApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeathersController : ControllerBase
+    {
+
+        WeatherContext context = new WeatherContext();
+
+        [HttpGet]
+        public IActionResult WeatherCityList()
+        {
+            var cities = context.Cities.ToList();
+            return Ok(cities);
+        }
+
+    }
+}
