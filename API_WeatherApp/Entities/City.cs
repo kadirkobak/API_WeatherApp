2ffdbd914508e3b14fef085319c30382aa73abe7
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-17 16:49:57 +0300

    Infrastructure completed.

---
+++ API_WeatherApp/Entities/City.cs
@@ -0,0 +1,13 @@
+﻿namespace API_WeatherApp.Entities
+{
+    public class City
+    {
+
+        public int CityId { get; set; }
+        public string CityName { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public decimal Temp { get; set; }
+        public string Detail { get; set; } = string.Empty;
+
+    }
+}
