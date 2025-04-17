acd61f631b62b597518474095d3635ef523e3b00
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-17 17:28:49 +0300

    Migrations completed.

---
+++ API_WeatherApp/Context/WeatherContext.cs
@@ -0,0 +1,17 @@
+﻿using API_WeatherApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_WeatherApp.Context
+{
+    public class WeatherContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=1LP021\\SQLEXPRESS;initial catalog=WeatherApiApp;integrated security=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<City> Cities { get; set; }
+
+    }
+}
